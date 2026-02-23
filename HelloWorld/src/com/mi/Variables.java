434d2838ed05153e165d4134f05ac36b7777a9cc
SajeedAI <training.saif@gmail.com>
2026-02-23 11:17:14 +0530

    v1

---
+++ HelloWorld/src/com/mi/Variables.java
@@ -0,0 +1,32 @@
+package com.mi;
+
+public class Variables {
+
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	
+	static float f;
+	static double d;
+	
+	static char ch;
+	
+	static boolean bb;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("byte: " + b);
+		System.out.println("short: " + s);
+		System.out.println("int: " + i);
+		System.out.println("long: " + l);
+		
+		System.out.println("float f: " + f);
+		System.out.println("dobule: " + d);
+		
+		System.out.println("char: " + ch);
+		
+		System.out.println("boolean: " + bb);
+	}
+}
