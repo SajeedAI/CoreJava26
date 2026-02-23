0f4c84dfa6de561e8a7e5335d336210a69fc9719
SajeedAI <training.saif@gmail.com>
2026-02-23 11:44:04 +0530

    v1

---
+++ HelloWorld/src/com/mi/DatatypesValues.java
@@ -0,0 +1,19 @@
+package com.mi;
+
+public class DatatypesValues {
+
+	public static void main(String[] args) {
+		
+		// Primitive data types: byte, short, int, long, float, double, char, boolean 
+		// Wrapper data types:   Byte, Short, Integer, Float, Double, Character, Boolean
+		
+		System.out.println(Byte.MIN_VALUE  + " " + Byte.MAX_VALUE); // -128 127
+		
+		System.out.println(Short.MIN_VALUE + " " + Short.MAX_VALUE); // -32768 32767
+		
+		System.out.println(Byte.TYPE); // byte
+		
+		
+		
+	}
+}
---
+++ HelloWorld/src/com/mi/Test.java
@@ -0,0 +1,15 @@
+package com.mi;
+
+public class Test {
+	
+	public Test() {
+		System.out.println("Default Constructor");
+	}
+
+	public static void main(String[] args) {
+		
+		new Test();
+		
+		System.out.println("Hello World");
+	}
+}
